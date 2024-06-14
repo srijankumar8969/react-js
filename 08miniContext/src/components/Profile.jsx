ac57ab42dde121ba3ee96240f08e1ff9860d58ad
Srijan kumar <147295283+srijankumar8969@users.noreply.github.com>
2024-06-14 15:11:37 +0530

    Update Profile.jsx

--- 08miniContext/src/components/Profile.jsx
+++ 08miniContext/src/components/Profile.jsx
@@ -2,7 +2,7 @@ import React, {useContext} from 'react'
 import UserContext from '../context/UserContext'
 
 function Profile() {
-  const {user} = useContext(UserContext)
+  const {user} = useContext(UserContext)  //we get access of the container //here we are directly using the data in the useConyext container provided from anywhere where the access of the container is available
 
   if(!user) return <h1>Not logged in </h1>
   return (
