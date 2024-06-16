baec608131b825c454198442f69a59668ec86333
Srijan kumar <147295283+srijankumar8969@users.noreply.github.com>
2024-06-16 16:22:14 +0530

    Update UserContextProvider.jsx

--- 08miniContext/src/context/UserContextProvider.jsx
+++ 08miniContext/src/context/UserContextProvider.jsx
@@ -4,7 +4,7 @@ import UserContext from './UserContext'
 const UserContextProvider= ({children}) => {
     const [user, setUser] = useState(null);
     return (
-        <UserContext.Provider value={{user, setUser}}>
+        <UserContext.Provider value={{user, setUser}}>  //user aur setuser dono pass kar diya taki agar values set karni hai ya //value ke ander is tarike se object ke form me hum variables daal dete hai jo ki use kiye ja sakte hain //ek baat yaad rakhna ki aap ek bari me context provide karte waqt hi sare variables ko declare  nahi kar pao ge as yo proceedd into the project you get the idea that wwhat things need to get added to context and what not// we add things to cintext so that they can be accssed at different places
             {children}
         </UserContext.Provider>
     )
