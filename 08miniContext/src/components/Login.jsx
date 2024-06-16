23cd815fc5ddac4b32cdd8fc8af4eecd6c37d154
Srijan kumar <147295283+srijankumar8969@users.noreply.github.com>
2024-06-16 16:27:57 +0530

    Update Login.jsx

--- 08miniContext/src/components/Login.jsx
+++ 08miniContext/src/components/Login.jsx
@@ -1,5 +1,5 @@
-import React, {useState, useContext} from 'react'
-import UserContext from '../context/UserContext'
+import React, {useState, useContext} from 'react';
+import UserContext from '../context/UserContext';
 
 function Login() {
     const [username, setUsername] = useState('')
@@ -14,9 +14,9 @@ function Login() {
   return (
     <div>
         <h2>Login</h2>
-        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder='Username' />
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder='Username' /> //
         {" "}
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Password' />
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Password' /> //
         <button onClick={handleSubmit}>Submit</button>
     </div>
   )
