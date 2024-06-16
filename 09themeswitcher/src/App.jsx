c2a42cda798fed217dcab6f7c3edf5b728eb1096
Srijan kumar <147295283+srijankumar8969@users.noreply.github.com>
2024-06-16 16:56:31 +0530

    Update App.jsx

--- 09themeswitcher/src/App.jsx
+++ 09themeswitcher/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   }, [themeMode])
 
   return (
-    <ThemeProvider value={{themeMode, darkTheme, lightTheme}}>
+    <ThemeProvider value={{themeMode, darkTheme, lightTheme}}> // jo bhi variable chahiye from context that can be accessed this way also.
       <div className="flex flex-wrap min-h-screen items-center">
           <div className="w-full">
               <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
