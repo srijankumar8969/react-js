093cb02d3da0d7806b9727354abe73eac8ad34e9
Srijan kumar <147295283+srijankumar8969@users.noreply.github.com>
2024-06-16 16:13:53 +0530

    Update UserContext.js

--- 08miniContext/src/context/UserContext.js
+++ 08miniContext/src/context/UserContext.js
@@ -1,5 +1,6 @@
 import React from "react";  //the container is first created and this is that creation file but we cant use this context to store or wrap our data we need to provide the container
 import {createContext} from 'react';
+
 const UserContext = createContext();
 
 export default UserContext;
