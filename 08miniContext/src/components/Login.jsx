d0924bd059cf6a7906cd0246c2aa8f67f82dc6c0
Srijan kumar <147295283+srijankumar8969@users.noreply.github.com>
2024-06-16 16:06:43 +0530

    Update Login.jsx

--- 08miniContext/src/components/Login.jsx
+++ 08miniContext/src/components/Login.jsx
@@ -5,32 +5,19 @@ function Login() {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
 
-    const {setUser} = useContext(UserContext)  //we get the access of the container we can add or use the data in the container
+    const {setUser} = useContext(UserContext)  //we get the access of the container we can add or use the data in the container from useContext(contextnmae)
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        setUser({username, password}) //we add some values to the container
+        setUser({username, password}) //we add some values to the variable of the container container
     }
   return (
     <div>
         <h2>Login</h2>
-        <input 
-        type="text"
-        value={username}
-            
-        onChange={(e) => setUsername(e.target.value)}
-        placeholder='Username'
-        />
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder='Username' />
         {" "}
-        <input
-        type="password" 
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder='Password'
-        />
-        <button
-        onClick={handleSubmit}
-        >Submit</button>
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Password' />
+        <button onClick={handleSubmit}>Submit</button>
     </div>
   )
 }
