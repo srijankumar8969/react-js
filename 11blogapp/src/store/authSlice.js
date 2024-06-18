7934e1c675d23efe7afdb57b96881d0988ddfc37
Srijan kumar <147295283+srijankumar8969@users.noreply.github.com>
2024-06-18 14:38:29 +0530

    Update authSlice.js

--- 11blogapp/src/store/authSlice.js
+++ 11blogapp/src/store/authSlice.js
@@ -1,26 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-
 const initialState = {
     status: false,
     userData: null
-}
+};  //initial state of the authslice 
 
 const authSlice = createSlice({
     name: "auth",
     initialState,
     reducers: {
-        login: (state, action) => {
+        login: (state, action) => {  // state authslice ke current state ko refer karta hai and action ke ander wo sab hai jo aap bhej rahe hai aur same name ke sath accessible bhi hai
             state.status = true;
-            state.userData = action.payload.userData
+            state.userData = action.payload.userData;
         },
         logout: (state) => {
             state.status = false;
-            state.userData = null
+            state.userData = null;
         }
     }
-})
+});
 
-export const { login, logout } = authSlice.actions
+export const { login, logout } = authSlice.actions;  //we need to export every thing in the reducer exclusively so that they can be accessed whenevr required
 
-export default authSlice.reducer
+export default authSlice.reducer;  //inside the authSlice it is reducers but here we use reducer only
