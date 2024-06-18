974608ab8652c5bea45040c1cee51ad3f85dd04a
Srijan kumar <147295283+srijankumar8969@users.noreply.github.com>
2024-06-19 00:09:24 +0530

    Update Input.jsx

--- 11blogapp/src/components/Input.jsx
+++ 11blogapp/src/components/Input.jsx
@@ -6,15 +6,10 @@ const Input = React.forwardRef( function Input({
     className= "",
     ...props}, 
     ref){
-        const id = useId()
+        const id = useId()  // a unique Id is assigned into the id variable
         return (
             <div className='w-full'>
-                {label && (
-                    <label htmlFor={id}
-                    className='inline-block mb-1 pl-1'>
-                        {label}
-                    </label>
-                )}
+                {label && (<label htmlFor={id} className='inline-block mb-1 pl-1'>{label}</label>)}
                 <input
                 className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
                 type={type}
