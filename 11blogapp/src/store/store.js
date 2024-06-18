f859cb4d411f24e1b9acd9c5df224ff64efa356e
Srijan kumar <147295283+srijankumar8969@users.noreply.github.com>
2024-06-18 16:12:08 +0530

    Update store.js

--- 11blogapp/src/store/store.js
+++ 11blogapp/src/store/store.js
@@ -3,8 +3,8 @@ import authSlice from "./authSlice"
 
 const store = configureStore({
     reducer: {
-        auth: authSlice,  //reducer is simply a js object and in this way we can pass as much key value pairs as we want
+        auth: authSlice,  //reducer is simply a js object and in this way we can pass as much key value pairs as we want and whatever is passed into it, it staets keeping track of it.
     }
 })
 
-export default store;
+export default store;  //complete store has to be exported
