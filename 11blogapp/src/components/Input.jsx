9263c9a4d372fb2eb70de9b09651719a74dcdc9a
Srijan kumar <147295283+srijankumar8969@users.noreply.github.com>
2024-06-22 15:35:58 +0530

    Update Input.jsx

--- 11blogapp/src/components/Input.jsx
+++ 11blogapp/src/components/Input.jsx
@@ -1,6 +1,6 @@
 import React, {useId} from 'react'
 
-const Input = React.forwardRef( function Input({
+const Input = React.forwardRef( function Input({  //input ka reference usko mil jayega and iske s
         label,
         type = 'text',
         className= "",
