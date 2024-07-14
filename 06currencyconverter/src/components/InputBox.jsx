3ab55b1f29ffa492d4514e087374024fc7d4ca3c
Srijan kumar <147295283+srijankumar8969@users.noreply.github.com>
2024-07-14 15:03:29 +0530

    Update InputBox.jsx

--- 06currencyconverter/src/components/InputBox.jsx
+++ 06currencyconverter/src/components/InputBox.jsx
@@ -32,7 +32,7 @@ function InputBox({
         <select 
         className='rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none'
         value={selectedCurrency}
-        onChange={(e) => { onCurrencyChange && onCurrencyChange(e.target.value)}}
+        onChange={(e) => {onCurrencyChange && onCurrencyChange(e.target.value)}}
         disabled={currrencyDisabled}
         >
             {currencyOptions.map((currency) => (
