68c43e4bf26ef93a7e72e5beb4dcf821757283e6
Srijan kumar <147295283+srijankumar8969@users.noreply.github.com>
2024-07-14 19:41:15 +0530

    Update TodoForm.jsx

--- 10todocontextLocal/src/components/TodoForm.jsx
+++ 10todocontextLocal/src/components/TodoForm.jsx
@@ -5,18 +5,18 @@ import {useTodo} from "../contexts/TodoContext"
 function TodoForm() {
     const [todo, setTodo] = useState("")
     const {addTodo} = useTodo() //useTodo is th provider of all the functions in the todo
-
+    
     const add = (e) => {
         e.preventDefault(); 
         if (!todo) return;
-        addTodo({todo, completed: false});
+        addTodo({todo, completed: false}); //addTodo is a function declared somewhere else and as this component has the access of the contet and as it has used the useTodo method to import it thus 
         setTodo("");
     }
   return (
     <form onSubmit={add} className='flex'>
         <input type="text" placeholder='Write Todo...' className='w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5'  value={todo}
         onChange={(e) => setTodo(e.target.value)} />
-        <button type="submit" className='rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0'> Add </button>
+        <button type="submit" className='rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0'>Add</button>
     </form>
   )
 }
