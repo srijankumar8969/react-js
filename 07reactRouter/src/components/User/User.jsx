b5beaf6e3b010d2ec392cf0657d83d070eadfaf6
Srijan kumar <147295283+srijankumar8969@users.noreply.github.com>
2024-10-20 17:38:14 +0530

    Update User.jsx

--- 07reactRouter/src/components/User/User.jsx
+++ 07reactRouter/src/components/User/User.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {useParams} from "react-router-dom"
 
 function User() {
-  const {userid} = useParams()
+  const {userid} = useParams() //this is how we can extract path parameter from url
   return (
     <div className='bg-orange-500 text-black text-3xl text-center py-5'>User: {userid}</div>
   )
